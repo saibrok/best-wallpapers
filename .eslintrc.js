fad19d36f97cbd0b853fa37dde6744a38e6bab1c
Sergey Litvin <s.litvin@systtech.ru>
2021-05-20 11:49:22 +0200

    prepare

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    '@vue/prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:vue/essential',
+    'prettier',
+    'prettier/vue',
+  ],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
